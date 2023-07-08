bc621e21fc5abe6d2518eaa8a23379221a4b9478
ngm <ngmaibulat@gmail.com>
2023-07-08 19:43:42 +0300

    Updated CI/CD

--- ngmapi/app.py
+++ ngmapi/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def index():
     # return render_template('index.html')
     # return redirect(url_for('login'))
-    return "Salam\n"
+    return "Salam!\n"
 
 
 # app.run(debug=True)
--- scripts/deployment/deploy-ubuntu.sh
+++ scripts/deployment/deploy-ubuntu.sh
@@ -1,3 +1,16 @@
+service_name="ngmapi"
+
+if systemctl list-units --full --all | grep -Fq "$service_name.service"; then
+    echo "Service $service_name exists"
+    git config --global --add safe.directory /opt/ngmapi
+    cd /opt/ngmapi
+    git pull
+    sudo systemctl restart ngmapi.service
+    exit 0
+else
+    echo "Service $service_name does not exist"
+fi
+
 echo 'export PS1="\[\e[1;32m\]\u@\h:\w\$\[\e[0m\] "' >> ~/.bashrc
 
 sudo apt -y install python3-pip
